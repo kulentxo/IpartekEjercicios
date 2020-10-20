e98b798c71e6c8b89aa4ce22b6a881d944b04038
kulen <kulen@DESKTOP-AA49AD2>
2020-10-20 09:35:01 +0200

    Insertado el enunciado

--- src/com/ipartek/ejercicios/excepciones/Ejercicio5.java
+++ src/com/ipartek/ejercicios/excepciones/Ejercicio5.java
@@ -2,6 +2,26 @@ package com.ipartek.ejercicios.excepciones;
 
 import com.ipartek.pojo.excepcionesEjercicio4Clase;
 
+/**
+ * Realizar un programa que lance un número aleatorio del 1 al 6.<br>
+ * Según el número sacado, se lanzara una excepción u otra de la siguiente
+ * lista:<br>
+ * <ul>
+ * <li>1: ArithmeticException
+ * <li>2: NullpointerException
+ * <li>3: NumberFormatException
+ * <li>4: SecurityException
+ * <li>5: NegativeArrayException
+ * <li>6: IOException
+ * </ul>
+ * Haciendo uso del try y del catch, mostrar un mensaje por pantalla y explicar
+ * cuál es la excepción ocurrida.<br>
+ * Repetir este proceso de manera indefinida, controlando dichas excepciones y
+ * sin dar pie a que el programa se cuelgue.
+ * 
+ * @author kulen
+ *
+ */
 public class Ejercicio5 {
 
 	final static int LIMITE_RAND = 6;
